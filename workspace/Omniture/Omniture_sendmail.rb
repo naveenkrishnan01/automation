af325f89ffcbda8bf559b8fd9f88b8b575efd415
nkrishnan <naveenkrishnan01@gmail.com>
2013-12-10 16:23:02 -0800

    mask email address and password

---
+++ workspace/Omniture/Omniture_sendmail.rb
@@ -0,0 +1,36 @@
+require 'rubygems'
+require 'time'
+require 'mail'
+
+fileName= File.open("./output1.txt")
+  
+  begin
+    File.exists?(fileName)
+       fileName.close
+  end     
+
+options = { :address => "smtp.gmail.com",
+	    :port => 587,
+            :domain => 'gmail.com',       
+            :user_name => 'your_username@gmail.com',
+            :password => 'your password',
+	    :authentication => 'plain',
+	    :enable_starttls_auto => true }
+
+
+
+Mail.defaults do
+delivery_method :smtp, options
+end
+
+
+Mail.deliver do
+    # 	to ['user1_emailaddress1', 'user2_emailaddress2']
+ 	cc 'naveenaddress1'
+	from 'your email address'
+        subject 'Automation Regression Complete for Omniture tracking. Scriptname : run-omniture.rb '
+         body 'Omniture tracking scripts.
+	 Please check the output file which has extracted all the Omniture variables and the pages clicked abd total clicks'
+        add_file '/Users/nkrishna/workspace/Omniture/output1.txt'
+end
+
